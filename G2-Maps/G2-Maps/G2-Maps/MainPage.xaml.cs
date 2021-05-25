7ef2fdce4de86ddf4791afc3c6b0e5ef6cabf8a3
simonejordan03 <simonejordan03@gmail.com>
2021-05-25 09:19:27 +0200

    Created Project with map, Removed unused files

---
+++ G2-Maps/G2-Maps/G2-Maps/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace G2_Maps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Position positionItaly = new Position(42.97197087627496, 12.553520745277323);
+            MapSpan mapSpan = MapSpan.FromCenterAndRadius(positionItaly, Distance.FromKilometers(500));
+            viewMap.MoveToRegion(mapSpan);
+
+            Pin pin = new Pin
+            {
+                Label = "Italia",
+                Address = "Click for more info",
+                Position = positionItaly
+            };
+
+            pin.InfoWindowClicked += async (s, args) =>
+            {
+                string pinName = ((Pin)s).Label;
+                await DisplayAlert("Info Window Clicked", $"Info of {pinName}.", "Ok");
+            };
+
+            //pin.MarkerClicked += async (s, args) =>
+            //{
+            //    string pinName = ((Pin)s).Label;
+            //    await DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
+            //};
+
+
+            viewMap.Pins.Add(pin);
+        }
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# G2-Maps-Repo
-Android application with Google-Maps API
-
-
-# Features
-Work in progress...
